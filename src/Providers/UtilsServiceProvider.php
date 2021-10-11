aeaa175bc0978a0eeb8a52d7c3822e81742af3ee
Morten Harders <mh@coolrunner.dk>
2021-10-11 11:25:28 +0200

    Update

--- src/Providers/UtilsServiceProvider.php
+++ src/Providers/UtilsServiceProvider.php
@@ -1,5 +1,5 @@
 <?php
-namespace CoolRunner\Utils;
+namespace CoolRunner\Utils\Providers;
 
 use CoolRunner\Utils\Http\Middleware\InputLogger;
 use CoolRunner\Utils\Models\InLog;
@@ -10,21 +10,18 @@ class UtilsServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        $this->loadMigrationsFrom(__DIR__ . '/Database/Migrations');
         $this->mergeConfigFrom(__DIR__ . '/config/config.php', 'utils');
     }
 
     public function boot()
     {
         if ($this->app->runningInConsole()) {
-
             $this->publishes([
                 __DIR__ . '/config/config.php' => config_path('CoolRunnerUtils.php'),
             ], 'config');
-
         }
 
+        // Register router middlewares
         $router = $this->app->make(Router::class);
-        $router->aliasMiddleware('input_log', InputLogger::class);
     }
 }
